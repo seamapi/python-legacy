8b7dedf62fa6cb87de47cc73370813944d58fc03
seveibar <seveibar@gmail.com>
2022-07-30 12:15:28 -0700

    workflow fix, fix test_devices

--- tests/devices/test_devices.py
+++ tests/devices/test_devices.py
@@ -8,10 +8,9 @@ def test_devices(seam: Seam):
 
     devices = seam.devices.list()
     assert len(devices) > 0
-    print("devices", devices)
 
-    some_device = seam.devices.get(name="FRONT DOOR")
-    assert some_device.properties.name == "FRONT DOOR"
+    some_device = seam.devices.get(name="Generated Lock 0")
+    assert some_device.properties.name == "Generated Lock 0"
 
     locks = seam.locks.list()
     assert len(locks) > 0
@@ -19,11 +18,12 @@ def test_devices(seam: Seam):
     some_lock = seam.locks.get(device=(some_device))
     assert some_lock.device_id == some_device.device_id
 
-    assert some_lock.properties.locked == False
-    seam.locks.lock_door(device=(some_device))
-    some_locked_lock = seam.locks.get(device=(some_device))
-    assert some_locked_lock.properties.locked == True
+    assert some_lock.properties.locked == True
 
     seam.locks.unlock_door(device=(some_device.device_id))
     some_unlocked_lock = seam.locks.get(device=(some_device))
     assert some_unlocked_lock.properties.locked == False
+
+    seam.locks.lock_door(device=(some_device))
+    some_locked_lock = seam.locks.get(device=(some_device))
+    assert some_locked_lock.properties.locked == True
