ce5afdadccedd4b82fd65a3641334f673d5b5257
Evan Sosenko <evan@getseam.com>
2022-11-15 15:43:48 -0800

    Update test for duplicate code error

--- tests/access_codes/test_access_codes.py
+++ tests/access_codes/test_access_codes.py
@@ -1,5 +1,5 @@
 from seamapi import Seam
-from seamapi.types import ActionAttemptFailedException
+from seamapi.types import SeamAPIException
 from tests.fixtures.run_august_factory import run_august_factory
 import pytest
 
@@ -21,7 +21,7 @@ def test_access_codes(seam: Seam):
     access_code = seam.access_codes.get(created_access_code.access_code_id)
     assert access_code.code == "4444"
 
-    with pytest.raises(ActionAttemptFailedException):
+    with pytest.raises(SeamAPIException):
         seam.access_codes.create(some_device.device_id, "Duplicate Access Code", "4444")
 
     access_code = seam.access_codes.update(access_code, name="Updated name")
