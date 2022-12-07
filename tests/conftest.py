65aa17f434761164d26a1b7f8ec1e00e6e73170d
Evan Sosenko <evan@getseam.com>
2022-12-06 16:28:02 -0800

    Use pg.get_container_host_ip and pg.get_connection_url (#65)

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,22 +28,28 @@ class SeamBackend:
 @pytest.fixture(scope="function")
 def seam_backend():
     with PostgresContainer("postgres:13", dbname="postgres") as pg:
-        db_host = (
-            "host.docker.internal"
-            if sys.platform == "darwin"
-            else "172.17.0.1"
-        )
-        db_url = f"postgresql://test:test@{db_host}:{pg.get_exposed_port(pg.port_to_expose)}/postgres"
+        db_host = pg.get_container_host_ip()
+        db_url = pg.get_connection_url()
+
+        # UPSTREAM: https://github.com/testcontainers/testcontainers-python/issues/159
+        docker_info = pg.get_docker_client().client.info()
+        if docker_info["OperatingSystem"] == "Docker Desktop":
+            container_host = "host.docker.internal"
+            db_url = db_url.replace(db_host, container_host)
+            db_host = container_host
+
         with DockerContainer(
             os.environ.get(
                 "SEAM_CONNECT_IMAGE", "ghcr.io/seamapi/seam-connect"
             )
-        ).with_env("DATABASE_URL", db_url,).with_env(
+        ).with_env(
+            "DATABASE_URL", db_url
+        ).with_env(
             "POSTGRES_DATABASE", "postgres"
         ).with_env(
             "NODE_ENV", "test"
         ).with_env(
-            "POSTGRES_HOST", db_host
+            "POSTGRES_HOST", db_host,
         ).with_env(
             "SERVER_BASE_URL", "http://localhost:3020"
         ).with_env(
