4c62f7c89d4f3c2e9ae43e29df86f466df34135c
Evan Sosenko <evan@getseam.com>
2022-12-30 11:23:04 -0600

    Add run_salto_factory

---
+++ tests/fixtures/run_salto_factory.py
@@ -0,0 +1,15 @@
+from seamapi import Seam
+import time
+import requests
+
+
+def run_salto_factory(seam: Seam):
+    seam.make_request(
+        "POST",
+        "/internal/scenarios/factories/load",
+        json={
+            "factory_name": "create_salto_devices",
+            "input": {"num": 1},
+            "sync": True,
+        },
+    )
