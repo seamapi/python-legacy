01b9ecbbcc731751f998d193230971687d71852c
Pawel <pawelstepniak22@gmail.com>
2023-11-30 17:23:39 +0000

    add generate in development mode

--- generate.sh
+++ generate.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 mkdir -p ./generated
-npx nextlove-sdk-generator generate python ./generated
 
-rm -rf ./seamapi
-cp -r ./generated/seamapi ./seamapi
-
-# 2. Why is node not recognized inside the container?
+if [ "$1" == "--dev" ]; then
+  # Use the nextlove-sdk-generator from the parent directory
+  cd ../nextlove-sdk-generator && npm run generate:python-sdk
+  rm -rf ./seamapi
+  cp -r ../nextlove-sdk-generator/output/python/seamapi ./seamapi
+else
+  # Use Global Nextlove SDK Generator
+  npx nextlove-sdk-generator generate python ./generated
+  rm -rf ./seamapi
+  cp -r ./generated/seamapi ./seamapi
+fi
