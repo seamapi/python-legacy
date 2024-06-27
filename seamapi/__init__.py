370467a2645d31d783805e83279fb3a26ee794c5
Evan Sosenko <evan@getseam.com>
2024-06-27 14:13:50 -0700

    Raise runtime error on import

--- seamapi/__init__.py
+++ seamapi/__init__.py
@@ -1,9 +1,7 @@
 # flake8: noqa
 # type: ignore
 
-import warnings
-
-warnings.warn("The seamapi package has been moved to seam. \n Please refer to the migration guide at https://github.com/seamapi/python/releases/tag/v1.0.0", DeprecationWarning)
+raise RuntimeError("The seamapi package has been moved to seam. \n Please refer to the migration guide at https://github.com/seamapi/python/releases/tag/v1.0.0")
 
 from seamapi.seam import Seam
 from seamapi.seam import SeamApiException
